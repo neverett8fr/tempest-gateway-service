ab6ec1ce004418812aaa19e7140410a45fa58f09
neverett8fr <neverett8fr@btinternet.com>
2022-11-03 03:05:02 +0000

    test remove port

--- pkg/infra/external/get.go
+++ pkg/infra/external/get.go
@@ -9,7 +9,7 @@ import (
 
 func Get(req entities.GetRequest) (*application.Response, error) {
 
-	resp, err := http.Get(fmt.Sprintf("%s://%s:%v/%s", req.Protocol, req.Host, req.Port, req.Route))
+	resp, err := http.Get(fmt.Sprintf("%s://%s/%s", req.Protocol, req.Host, req.Route))
 	if err != nil {
 		return nil, fmt.Errorf("error calling service, err %v", err)
 	}
