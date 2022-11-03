c88c769df7bd6bd9614fd7bced1eb137cbffb0d1
neverett8fr <neverett8fr@btinternet.com>
2022-11-03 03:11:44 +0000

    fix port issue, dont include when not needed

--- pkg/infra/external/get.go
+++ pkg/infra/external/get.go
@@ -9,7 +9,12 @@ import (
 
 func Get(req entities.GetRequest) (*application.Response, error) {
 
-	resp, err := http.Get(fmt.Sprintf("%s://%s/%s", req.Protocol, req.Host, req.Route))
+	route := fmt.Sprintf("%s://%s/%s", req.Protocol, req.Host, req.Route)
+	if req.Port != 0 {
+		route = fmt.Sprintf("%s://%s:%v/%s", req.Protocol, req.Host, req.Port, req.Route)
+	}
+
+	resp, err := http.Get(route)
 	if err != nil {
 		return nil, fmt.Errorf("error calling service, err %v", err)
 	}
