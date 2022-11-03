c3022d5ffb1a775c17484cf855325d1f237c4459
neverett8fr <neverett8fr@btinternet.com>
2022-11-03 17:24:49 +0000

    added in multiple method support 'POST', auth not added yet

--- pkg/infra/external/post.go
+++ pkg/infra/external/post.go
@@ -1,20 +1,28 @@
 package external
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	application "tempest-gateway-service/pkg/application/entities"
 	"tempest-gateway-service/pkg/infra/entities"
 )
 
-func Get(req entities.Request) (*application.Response, error) {
+func Post(req entities.Request) (*application.Response, error) {
 
 	route, err := constructRoute(req)
 	if err != nil {
 		return nil, fmt.Errorf("error constructing route, err %v", err)
 	}
 
-	resp, err := http.Get(route)
+	body := bytes.Buffer{}
+	err = json.NewEncoder(&body).Encode(req.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error encoding body, err %v", err)
+	}
+
+	resp, err := http.Post(route, req.ContentType, &body)
 	if err != nil {
 		return nil, fmt.Errorf("error calling service, err %v", err)
 	}
