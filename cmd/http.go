4ffce8d2c760e09e3d6e0dff9ce234ad92cb167e
neverett8fr <neverett8fr@btinternet.com>
2023-03-03 06:36:46 +0000

    change to add cors

--- cmd/http.go
+++ cmd/http.go
@@ -5,20 +5,31 @@ import (
 	"log"
 	"net/http"
 	"tempest-gateway-service/pkg/config"
-	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func StartServer(conf *config.Service, router *mux.Router) error {
-	srv := &http.Server{
-		Handler:      router,
-		Addr:         fmt.Sprintf("%v:%v", conf.Host, conf.Port),
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
+	// srv := &http.Server{
+	// 	Handler:      router,
+	// 	Addr:         fmt.Sprintf("%v:%v", conf.Host, conf.Port),
+	// 	WriteTimeout: 15 * time.Second,
+	// 	ReadTimeout:  15 * time.Second,
+	// }
+	// log.Printf("Server started on port: %v", conf.Port)
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+	})
+
+	log.Fatal(http.ListenAndServe(
+		fmt.Sprintf("%v:%v", conf.Host, conf.Port),
+		c.Handler(router),
+	))
 	log.Printf("Server started on port: %v", conf.Port)
 
-	log.Fatal(srv.ListenAndServe())
+	// log.Fatal(srv.ListenAndServe())
 	return nil
 }
