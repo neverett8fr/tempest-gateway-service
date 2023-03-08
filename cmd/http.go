fcf8d6708b5fbf53ac16479f49853076e4ecc52e
neverett8fr <neverett8fr@btinternet.com>
2023-03-08 15:29:15 +0000

    change cors library

--- cmd/http.go
+++ cmd/http.go
@@ -5,12 +5,18 @@ import (
 	"log"
 	"net/http"
 	"tempest-gateway-service/pkg/config"
+	"time"
 
-	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func StartServer(conf *config.Service, router *mux.Router) error {
+
+	// methodsAllowed := handlers.AllowedMethods([]string{"*"})
+	// originsAllowed := handlers.AllowedOrigins([]string{"*"})
+	// headersAllowed := handlers.AllowedHeaders([]string{"*"})
+
 	// srv := &http.Server{
 	// 	Handler:      router,
 	// 	Addr:         fmt.Sprintf("%v:%v", conf.Host, conf.Port),
@@ -19,18 +25,24 @@ func StartServer(conf *config.Service, router *mux.Router) error {
 	// }
 	// log.Printf("Server started on port: %v", conf.Port)
 
-	methodsAllowed := handlers.AllowedMethods([]string{"*"})
-	originsAllowed := handlers.AllowedOrigins([]string{"*"})
-	headersAllowed := handlers.AllowedHeaders([]string{"*"})
+	handler := cors.Default().Handler(router)
 
-	log.Fatal(http.ListenAndServe(
-		fmt.Sprintf("%v:%v", conf.Host, conf.Port),
-		handlers.CORS(
-			methodsAllowed,
-			originsAllowed,
-			headersAllowed,
-		)(router),
-	))
+	srv := &http.Server{
+		Handler:      handler,
+		Addr:         fmt.Sprintf("%v:%v", conf.Host, conf.Port),
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	// log.Fatal(http.ListenAndServe(
+	// 	fmt.Sprintf("%v:%v", conf.Host, conf.Port),
+	// 	handlers.CORS(
+	// 		methodsAllowed,
+	// 		originsAllowed,
+	// 		headersAllowed,
+	// 	)(router),
+	// ))
+	log.Fatal(srv.ListenAndServe())
 	log.Printf("Server started on port: %v", conf.Port)
 
 	// log.Fatal(srv.ListenAndServe())
