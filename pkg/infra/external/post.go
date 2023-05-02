a9d17e86ac53b7e956c700b032299c924ebce6ef
neverett8fr <neverett8fr@btinternet.com>
2023-05-02 21:47:00 +0100

    logs & docker-compose update with networks, etc.

--- pkg/infra/external/post.go
+++ pkg/infra/external/post.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	application "tempest-gateway-service/pkg/application/entities"
 	"tempest-gateway-service/pkg/infra/entities"
@@ -11,19 +12,24 @@ import (
 
 func Post(req entities.Request) (*application.Response, error) {
 
+	log.Println("post request received")
+
 	route, err := constructRoute(req)
 	if err != nil {
+		log.Printf("error, %v", err)
 		return nil, fmt.Errorf("error constructing route, err %v", err)
 	}
 
 	body := bytes.Buffer{}
 	err = json.NewEncoder(&body).Encode(req.Body)
 	if err != nil {
+		log.Printf("error, %v", err)
 		return nil, fmt.Errorf("error encoding body, err %v", err)
 	}
 
 	request, err := http.NewRequest(http.MethodPost, route, &body)
 	if err != nil {
+		log.Printf("error, %v", err)
 		return nil, fmt.Errorf("error building request, err %v", err)
 	}
 
@@ -33,8 +39,10 @@ func Post(req entities.Request) (*application.Response, error) {
 	client := &http.Client{}
 	resp, err := client.Do(request)
 	if err != nil {
+		log.Printf("error, %v", err)
 		return nil, fmt.Errorf("error calling service, err %v", err)
 	}
 
+	log.Println("reading post response")
 	return readBody(*resp)
 }
