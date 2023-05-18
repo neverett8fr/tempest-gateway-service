81c9092eaa6f27e4cae221286e97cc07eb2e4193
neverett8fr <neverett8fr@btinternet.com>
2023-05-18 02:11:09 +0100

    logging

--- pkg/infra/external/utils.go
+++ pkg/infra/external/utils.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	application "tempest-gateway-service/pkg/application/entities"
 	"tempest-gateway-service/pkg/infra/entities"
@@ -22,12 +23,14 @@ func readBodyToJson(resp http.Response) (*application.Response, error) {
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
+		log.Printf("error reading body, err %v", err)
 		return nil, fmt.Errorf("error reading body, err %v", err)
 	}
 
 	applicationResponse := application.Response{}
 	err = json.Unmarshal(body, &applicationResponse)
 	if err != nil {
+		log.Printf("error reading body, err %v", err)
 		return nil, fmt.Errorf("error unmarshalling body, err %v", err)
 	}
 
@@ -38,6 +41,7 @@ func readBody(resp http.Response) (interface{}, error) {
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
+		log.Printf("error reading body, err %v", err)
 		return nil, fmt.Errorf("error reading body, err %v", err)
 	}
 
